0cdc913bda1e52b587ea6ea806ca7989abb82d8c
ghimdohyun <ghimdohyun@gmail.com>
2026-01-19 21:37:45 +0900

    chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,11 +1,4 @@
 export default async function handler(req, res) {
-  return res.status(200).json({
-    ok: true,
-    method: req.method,
-    hasKey: !!process.env.GROQ_API_KEY,
-  });
-}
-export default async function handler(req, res) {
   // CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
